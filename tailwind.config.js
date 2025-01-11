79a7137ea3ac6d899af226a061ce9784d8cdb493
sabarish-10 <sabarishcse2023@ksrce.ac.in>
2025-01-11 16:26:24 +0000

    feat:added router package

--- tailwind.config.js
+++ tailwind.config.js
@@ -23,6 +23,15 @@ export default {
       fontFamily: {
         segoe: ['"Segoe UI"', 'sans-serif'], 
       },
+      backgroundImage: {
+        'custom-radial': 'radial-gradient(circle,rgb(245, 110, 0),rgb(27, 14, 1),rgb(12, 8, 3))',
+        'radial-gradient': 'radial-gradient(circle, var(--tw-gradient-stops))',
+        'radial-at-t': 'radial-gradient(circle at top, var(--tw-gradient-stops))',
+        'radial-at-b': 'radial-gradient(circle at bottom, var(--tw-gradient-stops))',
+        'radial-at-l': 'radial-gradient(circle at left, var(--tw-gradient-stops))',
+        'radial-at-r': 'radial-gradient(circle at right, var(--tw-gradient-stops))',
+        'grid': 'repeating-linear-gradient(45deg, transparent, transparent 20px, rgba(255, 255, 255, 0.1) 21px), repeating-linear-gradient(135deg, transparent, transparent 20px, rgba(255, 255, 255, 0.1) 21px)',
+      },
       fontSize: {
         'custom-base': '28px',
         'custom-md': '32px',
